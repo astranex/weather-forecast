58194fb0e9a93159f0bfc0e3619b6abdc9789f39
Галюзин Виталий Юрьевич <v.galyuzin@lugacom.local>
2022-05-24 10:10:08 +0300

    finished the primary version of the application

--- first/js/script.js
+++ first/js/script.js
@@ -3,33 +3,41 @@ const link = 'http://api.weatherstack.com/current?access_key=910defd469584ba0a43
 // language = ru русский язык API поддерживается только на профессиональном тарифном плане
 
 const root = document.querySelector('#root')
+const popup = document.querySelector('#popup')
+const textInput = document.querySelector('#text-input')
+const form = document.querySelector('#form')
+const close = document.querySelector('#close')
 
 let storage = {
    city: 'Luhansk',
-   feelslike: 0,
-   cloudcover: 0,
    temperature: 0,
-   humidity: 0,
    observationTime: '00:00 AM',
-   pressure: 0,
-   uvIndex: 0,
-   visibility: 0,
    isDay: 'yes',
    description: '',
-   windSpeed: 0,
+
+   properties: {
+      cloudcover: {},
+      humidity: {},
+      windSpeed: {},
+      pressure: {},
+      uvIndex: {},
+      visibility: {},
+
+   }
 }
 
-const fetchData = async () => { // Перед круглыми скобками пишем async (значит асинхронная функция)
-   const result = await fetch(`${link}&query=${storage.city}`) // Через & можем указать параметр
+const fetchData = async () => {
+   // Перед круглыми скобками пишем async (значит асинхронная функция)
+   const query = localStorage.getItem('query') || storage.city
+   const result = await fetch(`${link}&query=${query}`) // Через & можем указать параметр
    const data = await result.json()
 
    // console.log(data.current.feelslike)
    // console.log(data.request.language)
-   // console.log(data)
+   console.log(data)
 
    const { 
       current: { 
-         feelslike, 
          cloudcover, 
          temperature, 
          humidity, 
@@ -46,19 +54,49 @@ const fetchData = async () => { // Перед круглыми скобками 
 
    storage = {
       ...storage,
+      city: name,
       // feelslike: feelslike, // Сетаем для одноименного параметра storage его аналог из data // Одноименное слово можно не писать дважды
-      feelslike,
-      cloudcover,
       temperature,
-      humidity,
       // observationTime: observation_time, // В js приянто писать стилем camelCase, поэтому необходимо присвоить значение объектного словосочетания нашей переменной, но ещё проще задать будущее название переменной ещё при деструктуризации
-      observationTime,
-      pressure,
-      uvIndex,
-      visibility,
+      observationTime: observationTime.replaceAll('AM', ''),
+
       isDay,
       description: description[0], // Всегда берём нулевой элемент массива description изи объекта data
-      windSpeed,
+
+      properties: {
+         cloudcover: {
+            title: "Облачность",
+            value: `${cloudcover}%`,
+            icon: "cloud.png",
+          },
+          humidity: {
+            title: "Влажность",
+            value: `${humidity}%`,
+            icon: "humidity.png",
+          },
+          windSpeed: {
+            title: "Скорость ветра",
+            value: `${windSpeed} км/ч`,
+            icon: "wind.png",
+          },
+          pressure: {
+            title: "Давление",
+            value: `${pressure} Па`,
+            icon: "gauge.png",
+          },
+          uvIndex: {
+            title: "УФ-индекс",
+            value: `${uvIndex} / 100`,
+            icon: "uv-index.png",
+          },
+          visibility: {
+            title: "Видимость",
+            value: `${visibility}%`,
+            icon: "visibility.png",
+          },
+   
+      }
+
    }
 
    renderComponent()
@@ -71,7 +109,7 @@ const fetchData = async () => { // Перед круглыми скобками 
 
 }
 
-const getImage = (description) => {
+function getImage(description) {
    const value = description.toLowerCase();
  
    switch (value) {
@@ -90,8 +128,26 @@ const getImage = (description) => {
    }
 }
 
-const markup = () => {
-   const { city,  description, observationTime, temperature, isDay } = storage // Деструктуризация, создаём локальные переменные и берём значения из одноименных свойств объекта
+function renderProperty(properties) {
+
+   // console.log(properties)
+   return Object.values(properties).map(({ title, value, icon }) => { // Сразу в мапе деструктуризируем
+      // console.log(title, value, icon) // С помощью Object.entries возвращаем массив свойств необходимого объекта и работаем с ним через map
+ 
+      return `<div class="property">
+               <div class="property-icon">
+               <img src="./img/icons/${icon}" alt="">
+               </div>
+               <div class="property-info">
+               <div class="property-info__value">${value}</div>
+               <div id="google_translate_element" class="property-info__description">${title}</div>
+               </div>
+            </div>`
+      }).join('')
+}
+
+function markup() {
+   const { city,  description, observationTime, temperature, isDay, properties } = storage // Деструктуризация, создаём локальные переменные и берём значения из одноименных свойств объекта
 
    const containerClass = isDay === 'yes' ? 'is-day' : ''
 
@@ -99,7 +155,7 @@ const markup = () => {
             <div class="top">
             <div class="city">
                <div class="city-subtitle">Сегодня в городе</div>
-                  <div class="city-title" id="city">
+                  <div class="city-title" id="city" id="google_translate_element">
                   <span>${city}</span>
                </div>
             </div>
@@ -114,12 +170,45 @@ const markup = () => {
             </div>
             </div>
          </div>
-         <div id="properties"></div>
+         <div id="properties">${renderProperty(properties)}</div>
          </div>`
 }
 
-const renderComponent = () => {
+function renderComponent() {
    root.innerHTML = markup() // Вставляем заранее подготовленную вёрстку markup в приложение
+   
+   const city = document.querySelector('#city')
+   city.addEventListener('click', togglePopupClass)
 }
+
+function togglePopupClass() {
+   popup.classList.toggle('active')
+}
+
+function googleTranslateElementInit() {
+   new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ru'}, 'google_translate_element');
+ }
+
+function handleInput(e) {
+   storage = {
+      ...storage, // storage равняется одъекту storage, в котором свойство city равно e.target.value
+      city: e.target.value
+   }
+}
+
+function handleSubmit(e) {
+   e.preventDefault()
+
+   const value = storage.city
+   if (!value) return null // Если city не задано, возвращаем null
+
+   localStorage.setItem('query', value)
+   fetchData()
+   togglePopupClass()
+}
+
+form.addEventListener('submit', handleSubmit)
+textInput.addEventListener('input', handleInput)
+close.addEventListener('click', togglePopupClass)
 
 fetchData()
